39cb54fbf5cf420cbac8a9ecc415b7e709ad2f7a
nodirovich96 <nosirkhonuygunkhujaev@gmail.com>
2026-01-20 00:11:31 +0500

    Remove Telegram WebApp initialization code

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const statusEl = document.getElementById("status");
-  const userEl = document.getElementById("userid");
 
-  if (!window.Telegram || !window.Telegram.WebApp) {
-    statusEl.innerText = "❌ Открыто не из Telegram";
-  } else {
-    const tg = window.Telegram.WebApp;
-    tg.ready();
-    const user = tg.initDataUnsafe?.user;
-
-    if (user?.id) {
-      statusEl.innerText = "✅ Telegram Mini App открыт";
-      userEl.innerText = "User ID: " + user.id;
-    } else {
-      statusEl.innerText = "❌ user не передан";
-    }
-  }
-});
